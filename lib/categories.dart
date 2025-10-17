47d22bb8a209fd34deb08582558de194dfaf4a6d
Espérant KELA <esperant.kela@gmail.com>
2025-10-17 13:53:28 +0100

    first commit

---
+++ lib/categories.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Categories'),
+      ),
+      body: Center(
+        child: Text('Categories Screen'),
+      ),
+    );
+  }
+}
